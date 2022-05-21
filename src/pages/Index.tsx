e4a88f9087b9e7a8334586d527c3523d7269708f
Chris <42108459+impossible98@users.noreply.github.com>
2022-05-21 19:48:30 +0800

    fix: update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,5 +1,7 @@
-import { Layout } from '@arco-design/web-react';
-import { useEffect } from 'react';
+import { Button, Layout, Menu, Message, Typography } from '@arco-design/web-react';
+import { IconBook, IconHome } from '@arco-design/web-react/icon';
+import { Icon } from '@iconify/react';
+import { useEffect, useState } from 'react';
 
 import FooterIndex from '@/components/Footer/Index';
 import HomeIndex from '@/components/Home/Index';
@@ -9,11 +11,18 @@ const Sider = Layout.Sider;
 const Header = Layout.Header;
 const Footer = Layout.Footer;
 const Content = Layout.Content;
+const MenuItem = Menu.Item;
 
 function Index() {
+  const [collapsed, setCollapsed] = useState(false);
+
   useEffect(() => {
     document.title = `Index - ${TITLE}`;
   });
+
+  function handleCollapsed() {
+    setCollapsed(!collapsed);
+  }
 
   return (
     <Layout
@@ -28,10 +37,39 @@ function Index() {
           height: '70px',
         }}
       >
-        Header
+        <Button type='text' onClick={handleCollapsed}>
+          <Icon icon='ic:baseline-menu' />
+        </Button>
       </Header>
       <Layout>
-        <Sider>Sider</Sider>
+        <Sider
+          collapsed={collapsed}
+          onCollapse={handleCollapsed}
+          collapsible
+          trigger={null}
+          breakpoint='xl'
+        >
+          <div className='logo' />
+          <Menu
+            defaultOpenKeys={['1']}
+            defaultSelectedKeys={['0_3']}
+            onClickMenuItem={(key) => Message.info({ content: `You select ${key}`, showIcon: true })}
+            style={{ width: '100%' }}
+          >
+            <MenuItem key='0_1'>
+              <IconHome />
+              <Typography.Text>
+                Common
+              </Typography.Text>
+            </MenuItem>
+            <MenuItem key='0_2'>
+              <IconBook />
+              <Typography.Text>
+                Documentation
+              </Typography.Text>
+            </MenuItem>
+          </Menu>
+        </Sider>
         <Layout>
           <Content>
             <HomeIndex />
