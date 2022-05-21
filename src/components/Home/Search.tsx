53a6428e957ca49a305af453b5b25990336be410
Chris <42108459+impossible98@users.noreply.github.com>
2022-05-21 14:02:52 +0800

    fix: update Home/Search.tsx

--- src/components/Home/Search.tsx
+++ src/components/Home/Search.tsx
@@ -1,23 +1,37 @@
 import { Input } from '@chakra-ui/react';
+import { useState } from 'react';
 
 interface SearchProps {
   engine: string;
 }
 
 function Search(props: SearchProps) {
+  const [input, setInput] = useState(''); // input输入内容
+
   function search(e: any) {
     if (e.target.value === '') {
       return undefined;
-    } else if (e.keyCode === 13) {
-      window.open(props.engine + `${e.target.value}`);
     }
+    window.open(props.engine + `${e.target.value}`);
+  }
+
+  function handleKeyDown(e: any) {
+    if (e.keyCode === 13) {
+      search(e);
+    }
+  }
+
+  function handleChange(e: any) {
+    setInput(e.target.value);
   }
   return (
     <form action='.'>
       <Input
         focusBorderColor='pink.400'
+        value={input}
         type='search'
-        onKeyDown={search}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
       />
       <Input
         type='text'
