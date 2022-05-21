78ec68c6b076dcce4b4460d4b031ff4048538595
Chris <42108459+impossible98@users.noreply.github.com>
2022-05-21 16:02:19 +0800

    fix: update Index.tsx

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -1,10 +1,37 @@
+import { Layout } from '@arco-design/web-react';
+
 import HomeIndex from '@/components/Home/Index';
+
+const Sider = Layout.Sider;
+const Header = Layout.Header;
+const Footer = Layout.Footer;
+const Content = Layout.Content;
 
 function Index() {
   return (
-    <>
-      <HomeIndex />
-    </>
+    <Layout
+      style={{
+        height: '100vh',
+        minHeight: '568px',
+      }}
+    >
+      <Header
+        style={{
+          height: '70px',
+        }}
+      >
+        Header
+      </Header>
+      <Layout>
+        <Sider>Sider</Sider>
+        <Layout>
+          <Content>
+            <HomeIndex />
+          </Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+    </Layout>
   );
 }
 
